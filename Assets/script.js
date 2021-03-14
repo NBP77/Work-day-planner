0857f376bc6c05dae306884e43515c88a362ddc8
Nick Pendergast <nbp77@outlook.com>
2021-03-14 11:49:20 -0400

    Am able to store notes into local storage but cannot get text to stay on screen when applied

--- Assets/script.js
+++ Assets/script.js
@@ -3,3 +3,26 @@
 var today = moment();
 $('#currentDay').text(today.format('dddd'));
 
+const inpKey = document.getElementById("inpKey");
+const btnInsert = document.getElementById("btnInsert");
+
+renderLastRegistered();
+
+function renderLastRegistered() {
+  var notes = localStorage.getItem("inpKey");
+
+  inpKey.textContent = notes;
+
+}
+
+btnInsert.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var notes = document.querySelector('#inpKey').value;
+
+    localStorage.setItem("To do", notes);
+    renderLastRegistered();
+  }
+);
+;
+
