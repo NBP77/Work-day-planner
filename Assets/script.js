e8036233a5519e2dd472ae3a7f44cfd1a5c34253
NBP77 <nbp77@outlook.com>
2021-03-15 18:27:33 -0400

    putting final touches on what i can do for now, made a readme but cant get the other text boxes to stay on page

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,8 @@ $('#currentDay').text(today.format('dddd'));
 
 const inpKey = document.getElementById("inpKey");
 const btnInsert = document.getElementById("btnInsert");
+const btnInsert10 = document.getElementById("btnInsert10");
+const btnInsert12 = document.getElementById("btnInsert12")
 
 renderLastRegistered();
 
@@ -13,6 +15,9 @@ function renderLastRegistered() {
 
   inpKey.textContent = notes;
    $("#hour-9").val(localStorage.getItem("to-do"));
+   $("#hour-10").val(localStorage.getItem("hour10"));
+   $("#hour-12").val(localStorage.getItem("hour12"));
+
 
 }
 
@@ -29,5 +34,85 @@ btnInsert.addEventListener("click", function(event) {
 );
 ;
 
+btnInsert10.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var notes10 = document.querySelector('#hour-10').value;
+  console.log(notes10);
+
+    localStorage.setItem("hour10", notes10);
+    renderLastRegistered();
+  
+  }
+);
+;
+
+btnInsert10.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var notes12 = document.querySelector('#hour-12').value;
+  console.log(notes12);
+
+    localStorage.setItem("hour12", notes12);
+    renderLastRegistered();
+  
+  }
+);
+;
+
+
+
+
+
+// $(document).ready(function () {
+//   // listen for save button clicks
+//   $('.saveBtn').on('click', function () {
+//     // get nearby values
+//     var value = $(this).siblings('.description').val();
+//     var time = $(this).parent().attr('id');
+//     // save in localStorage
+//     localStorage.setItem(time, value);
+//     // Show notification that item was saved to localStorage by adding class 'show'
+//     $('.notification').addClass('show');
+//     // Timeout to remove 'show' class after 5 seconds
+//     setTimeout(function () {
+//       $('.notification').removeClass('show');
+//     }, 5000);
+//   });
+//   function hourUpdater() {
+//     // get current number of hours
+//     var currentHour = moment().hours();
+//     // loop over time blocks
+//     $('.time-block').each(function () {
+//       var blockHour = parseInt($(this).attr('id').split('-')[1]);
+//       // check if we've moved past this time
+//       if (blockHour < currentHour) {
+//         $(this).addClass('past');
+//       } else if (blockHour === currentHour) {
+//         $(this).removeClass('past');
+//         $(this).addClass('present');
+//       } else {
+//         $(this).removeClass('past');
+//         $(this).removeClass('present');
+//         $(this).addClass('future');
+//       }
+//     });
+//   }
+//   hourUpdater();
+//   // set up interval to check if current time needs to be updated
+//   var interval = setInterval(hourUpdater, 15000);
+//   // load any saved data from localStorage
+//   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+//   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+//   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+//   $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+//   $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+//   $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+//   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+//   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+//   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+//   // display current day on page
+  
+// });
 
 
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Work-day-planner
+A planner to help organize your workday
+
+## Description
+
+-The main goal for this project was to creat a work day planner that has the ability to store and save data on the page while giving you realtime time management.
+ 
+-When you enter the broswer you see the current day 
+ 
+-Moving down to the timeblocks you can see that there are organized by hours in the day. 
+
+-The user is able to click into a timeblock and write down notes. 
+
+-Currenlty working on the changing of the colors for each timeblock. 
+
+## Installation
+
+To check the project, I worked on click on these links: 
+
+GitHub: https://github.com/NBP77/Work-day-planner
+
+GitHub pages: https://nbp77.github.io/Work-day-planner/ 
+
+## Usage
+
+Examples:
+
+-Functional day display 
+
+-Organized hours on the side
+
+-Functioning save buttons and text area for notes 
+
+-Easy UI for smooth organizing
+
+-A great use of Bootstrap elements 
+
+-A great use of Moment.JS elements 
+
+## Credits
+
+I was able to do most of this based of the activites we have done in class but I think the setup of the html hurt me a bit when it came to making local storage consistent. 
